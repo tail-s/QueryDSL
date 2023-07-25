08082c64123598cdc60ccde9c22b7944c4e1985d
tail-s <lunar7s@naver.com>
2023-07-26 03:09:39 +0900

    23.07.26

---
+++ querydsl/src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,32 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
